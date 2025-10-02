2761f8868e10b9a0231618ed8cf9a5ff7ad2ba4c
6293167 <6293167@cstjean.qc.ca>
2025-10-02 14:39:35 -0400

    exercice

---
+++ 2 oct.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class interface(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Formulaire")
+        self.geometry("800x600")
+
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        self.creer_widgets()
+
+
+    def creer_widgets(self):
+        frm = tk.Frame(self, bg="mistyrose")
+        frm.grid(row=0, column=0, sticky="nsew")
+        frm.columnconfigure(0, weight=1)
+        frm.columnconfigure(1, weight=60)
+        frm.columnconfigure(2, weight=1)
+        frm.columnconfigure(3, weight=1)
+        frm.columnconfigure(4, weight=1)
+
+        frm.rowconfigure(2, weight=1)
+
+        nom = tk.Label(frm, text="Nom")
+        nom.grid(row=0, column=0, sticky="w", padx=20, pady=10)
+
+        email = tk.Label(frm, text = "Email")
+        email.grid(row = 0, column = 1, sticky = "w", padx=10, pady=10)
+
+        age = tk.Label(frm, text = "Age")
+        age.grid(row=0, column = 2, sticky = "w", padx=10, pady=10)
+
+        self.entrer_nom = tk.Entry(frm, width = 20)
+        self.entrer_nom.grid(row = 1, column = 0, sticky = "ew", padx=20, pady=10)
+
+        self.entrer_email = tk.Entry(frm, width = 30)
+        self.entrer_email.grid(row = 1, column = 1, sticky = "ew", padx=10, pady=10)
+
+        self.entrer_age = tk.Entry(frm, width = 10)
+        self.entrer_age.grid(row = 1, column = 2, sticky = "ew", padx=10, pady=10)
+
+        btn_ajouter = tk.Button(frm, text="Ajouter", command=self.ajouter_donnees)
+        btn_ajouter.grid(row = 1, column = 3, padx=10, pady=10)
+
+        btn_sup = tk.Button(frm, text="Supprimer sélection",command=self.supprimer_selection)
+        btn_sup.grid(row = 1, column = 4, padx=10, pady=10)
+
+        self.tableau =ttk.Treeview(frm, columns=("nom", "email", "age"), show="headings")
+        self.tableau.heading("nom", text="Nom")
+        self.tableau.heading("email", text="Email")
+        self.tableau.heading("age", text="Age")
+        self.tableau.column("nom", width=80)
+        self.tableau.column("email", width=200)
+        self.tableau.column("age", width=50)
+        self.tableau.grid(row=2, column=0,columnspan=5, sticky="nsew", padx=20, pady=(40,80))
+
+
+    def ajouter_donnees(self):
+        nom = self.entrer_nom.get().strip()
+        email = self.entrer_email.get().strip()
+        age = self.entrer_age.get().strip()
+
+        if nom and email and age:
+            self.tableau.insert("", "end", values=(nom, email, age))
+            # Vider les champs après l'ajout
+            self.entrer_nom.delete(0, tk.END)
+            self.entrer_email.delete(0, tk.END)
+            self.entrer_age.delete(0, tk.END)
+
+    def supprimer_selection(self):
+        selected_item = self.tableau.selection()
+        for item in selected_item:
+            self.tableau.delete(item)
+
+
+if __name__ == "__main__":
+    app = interface()
+    app.mainloop()
--- 30 sept.py
+++ 30 sept.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+from tkinter import messagebox
+
 
 class interface(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Gestionnaire de tâches")
         self.geometry("400x576")
+        self.tasks = []
 
         self.creer_widgets()
 
@@ -30,8 +33,8 @@ class interface(tk.Tk):
 
         durer = tk.Label(content, text = "Durée estimée de la tâche en minutes:")
         durer.grid(row=2, column=0, sticky="w", padx=10, pady=10)
-        self.champ_tache2 = tk.Entry(content)
-        self.champ_tache2.grid(row=2, column=1, sticky="ew", padx=10, pady=10)
+        self.champ_duree = tk.Entry(content)
+        self.champ_duree.grid(row=2, column=1, sticky="ew", padx=10, pady=10)
 
         self.prioritaire = tk.BooleanVar(value=False)
         self.cb_prioritaire = tk.Checkbutton(content, text = "Toutes les nouvelles tâche sont prioritaires:", variable=self.prioritaire)
@@ -45,13 +48,55 @@ class interface(tk.Tk):
         self.rb1.grid(row = 4, column = 1, sticky="w")
         self.rb2.grid(row = 5, column = 1, sticky="w")
 
-        self.bnt_ajouter = tk.Button(content, text = "Ajouter la tâche")
-        self.bnt_ajouter.grid(row = 6, column = 0,columnspan=2, pady = 30)
+        self.btn_ajouter = tk.Button(content, text = "Ajouter la tâche",command=self.ajouter_tache, state = "disabled")
+        self.btn_ajouter.grid(row = 6, column = 0,columnspan=2, pady = 30)
 
-        self.liste_tache = tk.Listbox(content, height = 15)
-        self.liste_tache.grid(row = 7, column = 0, columnspan = 2,sticky = "nsew",padx = 10,pady = 10)
+        self.liste_taches = tk.Listbox(content, height = 15)
+        self.liste_taches.grid(row = 7, column = 0, columnspan = 2,sticky = "nsew",padx = 10,pady = 10)
 
+    def valider_entree(self, nouvelle_valeur):
+        if (nouvelle_valeur.isdigit() and 0 < int(nouvelle_valeur) < 480):
+            return True
+        return False
 
+    def entree_invalide(self):
+        self.bell()  # Feedback discret :
+        messagebox.showwarning("EntrÃ©e invalide", "Veuillez entrer un entier positif")
+
+    def validate_tache(self, *args):
+        tache = self.champ_tache.get()
+        duree = self.champ_duree.get()
+
+        # Validation des champs
+        tache_valid = len(tache) > 0
+        duree_valid = len(duree) > 0
+
+        if tache_valid and duree_valid:
+            self.btn_ajouter.config(state="normal")
+        else:
+            self.btn_ajouter.config(state="disabled")
+
+    def ajouter_tache(self):
+        titre = self.champ_tache.get().strip()
+        if not titre:
+            messagebox.showwarning("Validation", "Veuillez saisir un titre de tache.")
+            return
+
+        task = {"titre": titre, "DurÃ©e": self.champ_duree.get(), "type": self.type_tache.get(),
+                "prioritaire": self.prioritaire.get()}
+        self.tasks.append(task)
+        self._refresh_listbox()
+
+    def _refresh_listbox(self):
+        self.liste_taches.delete(0, "end")
+        for t in self.tasks:
+            self.liste_taches.insert("end", self._format_task_for_list(t))
+
+    def _format_task_for_list(self, task):
+        p = "[P]" if task["prioritaire"] else ""
+        t = "[Prof]" if task["type"] == "Professionnel" else "[Pers]"
+
+        return f"{p}{t}[{task["DurÃ©e"]}] {task['titre']}".strip()
 
 
 
